0c044d2343b44ce5eba7ca04a1a606850967e314
FelixWattsYork <hmq514@york.ac.uk>
2024-12-19 13:51:34 +0000

    Sync

---
+++ miller.py
@@ -0,0 +1,21 @@
+from numpy import *
+import os
+print (os.getcwd())
+print (__file__)
+
+A=2.2
+kappa=1.5
+delta=0.3
+R0=2.5
+theta=linspace(0,2*pi)
+r=R0/A
+R_s=R0+r*cos(theta+(arcsin(delta)*sin(theta)))
+Z_s=kappa*r*sin(theta)
+
+import matplotlib.pyplot as plt
+
+plt.plot(R_s, Z_s)
+plt.axis("equal")
+plt.xlabel("R [m]")
+plt.ylabel("Z [m]")
+plt.savefig(os.getcwd() + "/miller.png")
